3892fef8faabe8f18ead672c3d4e2b6a589fc9d6
akfheaven <2454147892@qq.com>
2022-11-04 17:35:26 +0800

    Update cleaners.py

--- text/cleaners.py
+++ text/cleaners.py
@@ -98,3 +98,9 @@ def english_cleaners2(text):
   phonemes = phonemize(text, language='en-us', backend='espeak', strip=True, preserve_punctuation=True, with_stress=True)
   phonemes = collapse_whitespace(phonemes)
   return phonemes
+
+
+def chinese_cleaner(text):
+  phonemes = phonemize(text, language='cmn', backend='espeak', strip=True, preserve_punctuation=True, with_stress=True)
+  phonemes = collapse_whitespace(phonemes)
+  return phonemes

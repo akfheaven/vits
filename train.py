6a43b066acf5d8e36039bee06fc284cb9d6c1d95
akfheaven <2454147892@qq.com>
2022-11-04 18:16:23 +0800

    Update train.py

--- train.py
+++ train.py
@@ -222,8 +222,8 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
 
       if global_step % hps.train.eval_interval == 0:
         evaluate(hps, net_g, eval_loader, writer_eval)
-        utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "G_{}.pth".format(global_step)))
-        utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "D_{}.pth".format(global_step)))
+        utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "G_{}.pth".format("1000")))#trick:reduce free env mem
+        utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "D_{}.pth".format("1000")))
     global_step += 1
     logger.info('-------------> global_step{}, loss: {}'.format(global_step, loss_gen_all))
   if rank == 0:

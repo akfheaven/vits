2e9aefae6ed8a6c02f35f43e01bc20efede07f6f
akfheaven <2454147892@qq.com>
2022-11-04 17:35:55 +0800

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -8,9 +8,11 @@ _punctuation = ';:,.!?¡¿—…"«»“” '
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 
+_chinese_tone = "12345"
+_chinese_extra = "̪ -"
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + list(_chinese_tone) + list(_chinese_extra)
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
